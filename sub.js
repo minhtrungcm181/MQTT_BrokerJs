ec44e389cbfd0a9eea6bc9200f0385185bd7750d
minhtrungcm181 <minhtrungcm181@gmail.com>
2023-11-27 16:33:53 +0700

    finish

--- sub.js
+++ sub.js
@@ -1,14 +1,15 @@
 let mqtt = require('mqtt');
 var settings = {
     mqttServerUrl: "localhost",
-    port: 1500,
+    port: 1883,
     topic: "topic1"
 }
 var client = mqtt.connect('mqtt://' + settings.mqttServerUrl + ":" + settings.port);
 
 client.on('message', function (topic, message) {
+    topic = topic.toString()
     message = message.toString();
-    console.log(message);
+    console.log(topic + ' ' + message);
 })
 client.on('connect', () => {
     client.subscribe(settings.topic);

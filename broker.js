afd598beffc986698d7a4d5c754e6a782b3e8420
minhtrung <minhtrungcm181@gmail.com>
2024-05-26 14:58:45 +0700

    eit

--- broker.js
+++ broker.js
@@ -1,4 +1,5 @@
 let mosca = require('mosca');
+const pool = require('./dbconnector')
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const CSV_FILE = "mqtt_data.csv";
 let message;
@@ -27,7 +28,28 @@ function parseAndWriteToCsv(message) {
             console.error('Error writing data to CSV:', err);
         });
 }
-
+function formatTimestamp(timestamp) {
+    const day = String(timestamp.getDate()).padStart(2, '0');
+    const month = String(timestamp.getMonth() + 1).padStart(2, '0');
+    const year = timestamp.getFullYear();
+    const hour = String(timestamp.getHours()).padStart(2, '0');
+    const minute = String(timestamp.getMinutes()).padStart(2, '0');
+    const second = String(timestamp.getSeconds()).padStart(2, '0');
+  
+    return `${year}/${month}/${day} ${hour}:${minute}:${second}`;
+  }
+  var vol, cur, pow, trueP, kwh;
+  function insertDB(vol, cur, pow, trueP, kwh) {
+    if (vol > 0 && vol < 235 && kwh > 0){
+    const timestamp = new Date(); 
+    const formattedTimestamp = formatTimestamp(timestamp);
+    const query = `INSERT INTO power (timestamp, voltage, current, power, true_power, kwh)
+            VALUES ($1, $2, $3, $4, $5, $6)`;
+  
+          pool.query(query, [formattedTimestamp, vol, cur, pow, trueP, kwh]); 
+          console.log('Data inserted successfully');
+          console.log('------------------------');  }
+  } 
 var settings = {
     port: 1883,
     stats: false,
@@ -72,6 +94,19 @@ broker.on('published', (packet) => {
 // }
 
     message = packet.payload.toString();
+    if (topic == "topic5"){
+        dataString = message.replace(/"/g, '');
+        let values = dataString.split(',');
+   
+        vol = parseFloat(values[0]);
+        cur = parseFloat(values[1]);
+        pow = parseFloat(values[2]);
+        trueP = parseFloat(values[3]);
+        kwh = parseFloat(values[4]);
+      }
    
     console.log(topic + ' ' + message);
 })
+setInterval(() => {
+    insertDB(vol, cur, pow, trueP, kwh);
+  }, 90000);
--- pub.js
+++ pub.js
@@ -1,10 +1,10 @@
 let mqtt = require('mqtt');
 let client = mqtt.connect('mqtt://localhost:1883');
 
-// let topic = "topic5"
-// let message = '"221.7,120,19.02,30.4,10.0"'
-let topic = "topic6"
-let message = 'S'
+let topic = "topic5"
+let message = '"222.94,160,25.02,35.83,4"'
+// let topic = "topic6"
+// let message = 'S'
 
 
 client.on('connect', () => {
